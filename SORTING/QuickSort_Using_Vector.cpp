65a205710277093b246c220c7a91edaa7c01d5a1
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-05-05 10:53:51 +0530

    Create QuickSort_Using_Vector.cpp

---
+++ SORTING/QuickSort_Using_Vector.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int swap(int *a, int *b)
+{
+  int temp=*a;
+  *a=*b;
+  *b=temp;
+}
+int partition(vector<int> &arr, int start, int end)
+{
+  //to get the right position of pivot
+  int pivotindex = start;
+  //it can be any element lets say the end element
+  int pivot=arr[end];
+  for(int i=start;i<end;i++)
+  { 
+    //runs until.. arr[i]<last element
+    if(arr[i]<=pivot)
+    { 
+     //shifting all the smaller elements than pivot to left
+     swap(arr[i],arr[pivotindex]);
+     pivotindex++;
+    }
+  }
+  //swapping pivot element to its position
+  swap(arr[pivotindex],arr[end]);
+ return pivotindex;
+}
+
+int quicksort(vector<int> &arr, int start, int end)
+{ //until it becomes arr[7]
+  if(start<end)
+  {
+  int pindex=partition(arr, start, end);
+  //for sorting elements smaller than pivot
+  quicksort(arr, start, pindex-1);
+  //for sorting elements greater than pivot
+  quicksort(arr, pindex+1,end);
+  }
+  
+}
+int main()
+{  
+ //int arr[8]={7, 2, 1, 6, 8, 5, 3, 4};
+ vector<int> arr{7, 2, 1, 6, 8, 5, 3, 4};
+ quicksort(arr, 0, arr.size()-1);
+ for(int i=0;i<8;i++)
+ cout<<arr[i]<<endl;
+}
