f58b1473a47b118c5fd49063dca632c2e94ac487
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-07-31 08:31:02 +0530

    Update Linear_search.cpp
    Wrote Another Program

--- Searching/Linear_search.cpp
+++ Searching/Linear_search.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -22,4 +23,43 @@ int main()
   int el=34;
   linear(arr, n, el);
   return 0;
+}*/
+
+#include<iostream>
+using namespace std;
+
+int linearsearch( int *array, int sizeofarray, int tofind)
+{
+ for(int i = 0; i < sizeofarray; i++) 
+ {
+   if( array[i] == tofind)
+   cout<<"Element Found At Index "<<i<<endl;
+ }
+ cout<<"Finished!"<<endl;
 }
+
+int main()
+{
+  
+  cout<<"Enter The Size Of The Array "<<endl;
+  int sizeofarray;
+  cin>>sizeofarray;
+  
+  cout<<"Enter The Array Elements "<<endl;
+  int array[sizeofarray];
+  for(int i = 0; i < sizeofarray; i++)
+  {
+    cin>>array[i];
+  }
+  
+  cout<<"Enter The Number To Search For"<<endl;
+  int tofind;
+  cin>>tofind;
+  
+  //calling function to perform linear search
+  linearsearch(array, sizeofarray, tofind);
+  
+  return 0;
+}
+
+
